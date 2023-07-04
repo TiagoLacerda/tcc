bb07e0fc0bf1abb990ba5b3bff696e7ebc04aa3a
TiagoLacerda <tiagomlbrasil@gmail.com>
2023-07-03 23:18:20 -0300

    Added plotting script
    Added python script for plotting test data. Updated test case to generate larger volume of data.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #endif
 
+#ifndef FSTREAM
+#define FSTREAM
+#include <fstream>
+#endif
+
 #ifndef STRING
 #define STRING
 #include <string>
@@ -61,24 +66,9 @@ bool is_cyclic_disjoint_sets(Graph graph)
     return false;
 }
 
-int main()
+void add_cycle_inducing_edges(Graph *graph, unsigned int k)
 {
-    unsigned int n, k;
-
-    std::cout << "Enter the number of nodes in the graph: ";
-    std::cin >> n;
-
-    std::cout << "Enter the number of cycle-inducing edges to add: ";
-    std::cin >> k;
-
-    auto t0 = std::chrono::high_resolution_clock::now();
-
-    // Generate a random spanning tree with n nodes
-    auto graph = Graph::random_spanning_tree(n);
-
-    // Add k cycle-inducing edges
-
-    auto edges = Graph::inverse(graph).get_edges();
+    auto edges = Graph::inverse(*graph).get_edges();
 
     static std::random_device rd;
     static std::mt19937 rng(rd());
@@ -95,28 +85,48 @@ int main()
 
         edges.erase(edge);
 
-        graph.insert_edge(u, v);
+        (*graph).insert_edge(u, v);
+    }
+}
+
+int main()
+{
+    auto t0 = std::chrono::high_resolution_clock::now();
+
+    std::ofstream file("data/data.txt");
+
+    for (unsigned int n = 10; n <= 500; n += 10)
+    {
+        for (unsigned int k = 1; k <= 1; k++)
+        {
+            for (unsigned int sample = 0; sample < 20; sample++)
+            {
+                auto graph = Graph::random_spanning_tree(n);
+
+                add_cycle_inducing_edges(&graph, k);
+
+                auto t1 = std::chrono::high_resolution_clock::now();
+
+                bool result1 = graph.is_cyclic();
+
+                auto t2 = std::chrono::high_resolution_clock::now();
+
+                bool result2 = is_cyclic_disjoint_sets(graph);
+
+                auto t3 = std::chrono::high_resolution_clock::now();
+
+                auto duration_dfs = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+                auto duration_djs = std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2);
+
+                file << n << " " << k << " " << duration_dfs.count() << " " << duration_djs.count() << std::endl;
+            }
+        }
     }
 
-    //
+    file.close();
 
-    graph.to_file("graph.txt");
+    auto t4 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t0);
 
-    auto t1 = std::chrono::high_resolution_clock::now();
-
-    bool result1 = graph.is_cyclic();
-
-    auto t2 = std::chrono::high_resolution_clock::now();
-
-    bool result2 = is_cyclic_disjoint_sets(graph);
-
-    auto t3 = std::chrono::high_resolution_clock::now();
-
-    auto duration_dfs = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-    auto duration_djs = std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2);
-
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t0);
-    std::cout << "[DFS] Elapsed: " << duration_dfs.count() << " us, cyclic? " << (result1 ? "yes" : "no") << std::endl;
-    std::cout << "[DJS] Elapsed: " << duration_djs.count() << " us, cyclic? " << (result2 ? "yes" : "no") << std::endl;
     std::cout << "Elapsed: " << duration.count() << " ms" << std::endl;
 }
---
+++ main.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+file = open("data/data.txt", "r")
+data = [list(map(int, line.split(" "))) for line in file.read().splitlines()]
+
+n = [item[0] for item in data]
+dfs = [item[2] for item in data]
+djs = [item[3] for item in data]
+
+fig, ax = plt.subplots()
+
+plt.title("Tempo de execução dos algoritmos de detecção de ciclos", fontsize=10, fontweight='ultralight')
+plt.xlabel("Número de vértices do grafo")
+plt.ylabel("Tempo decorrido (us)")
+plt.tick_params(axis='both', which='major', labelsize=8)
+plt.grid(color='#efefff')
+# plt.rc('axes', axisbelow=True)
+ax.set_axisbelow(True)
+
+ax.spines[['left', 'top', 'right', 'bottom']].set_visible(False)
+
+ax.scatter(n, dfs, color='red', linewidth=1.0, label='Depth-First Search')
+ax.scatter(n, djs, color='blue', linewidth=1.0, label='Disjoint Sets')
+ax.legend()
+
+# plt.show()
+plt.savefig('data/data.png')
