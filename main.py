f48a6b915bdb19d8ca9da47b55da8eb1a9f856d1
Tiago Lacerda <tiagomlbrasil@gmail.com>
2024-12-09 09:18:21 -0300

    Updated auxiliary python file for plotting data

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import random
+import json
 
-# Load data from file
+with open("results.json", "r") as file:
+    data = json.load(file)
 
-file = open("data/data.txt", "r")
-data = [list(map(int, line.split(" "))) for line in file.read().splitlines()]
-
-n = [item[0] for item in data]
-dfs = [item[1] for item in data]
-djs = [item[2] for item in data]
-
-# Setup matplotlib
+# Setup
 
 fig, ax = plt.subplots()
 
-plt.title("Tempo de execução dos algoritmos de decisão de conexidade", fontsize=10, fontweight='ultralight')
-plt.xlabel("Número de vértices do grafo")
-# plt.xlabel("Número de arestas que induzem ciclo no grafo")
+plt.title("Tempo de execução dos algoritmos de geração de árvores-geradoras", fontsize=10, fontweight='ultralight')
+plt.xlabel("Número de threads")
 plt.ylabel("Tempo decorrido (us)")
 plt.tick_params(axis='both', which='major', labelsize=8)
 plt.grid(color='#efefff')
-# plt.rc('axes', axisbelow=True)
 ax.set_axisbelow(True)
-
 ax.spines[['left', 'top', 'right', 'bottom']].set_visible(False)
 
 # Plot data
 
-ax.scatter(n, dfs, color='red', linewidth=1.0, label='Depth-First Search')
-ax.scatter(n, djs, color='blue', linewidth=1.0, label='Disjoint Sets')
-ax.legend()
+for graph in data:
+    foo = dict()
+    
+    for execution in graph["executions"]:
+        threads = execution["threads"]
+        elapsed = execution["elapsed"]
+        if threads not in foo:
+            foo[threads] = []
+        foo[threads].append(elapsed)
 
-# plt.show()
-plt.savefig('data/data.png')
+    x = [k for k in foo]
+    y = [np.average(foo[k]) for k in foo]
+    e = [np.std(foo[k]) for k in foo]
+    ax.errorbar(x, y, linewidth=1.0, label=f"N={graph["n"]}, M={graph["m"]}", yerr=e, linestyle='None', marker='.')
+
+# Shift plot up to make room for legend
+box = ax.get_position()
+ax.set_position([box.x0, box.y0 + box.height * 0.2,
+                 box.width, box.height * 0.8])
+
+# Legend
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),
+           ncol=4, fontsize=8)
+
+
+plt.savefig('data.png')
