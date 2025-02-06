2b37f36983c0115de5559db34594a214a51a6c11
Tiago Lacerda <tiagomlbrasil@gmail.com>
2025-02-06 13:17:37 -0300

    Use sequential approach when targetting only 1 thread

--- main.cpp
+++ main.cpp
@@ -156,7 +156,14 @@ nlohmann::ordered_json evaluate(const std::string path, const bool debug, const 
 
             t0 = std::chrono::high_resolution_clock::now();
 
-            spanning_tree::generate_parallel(graph, callback, &abort, lower_bound, t);
+            if (t == 1)
+            {
+                spanning_tree::generate_sequential(graph, callback, &abort, lower_bound);
+            }
+            else
+            {
+                spanning_tree::generate_parallel(graph, callback, &abort, lower_bound, t);
+            }
 
             t1 = std::chrono::high_resolution_clock::now();
 
